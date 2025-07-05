687b38ec6d99db573765c32902240982cf3f5c70
Shravan765 <142580349+Shravan765@users.noreply.github.com>
2025-07-05 22:22:59 +0530

    Update leaderboard.py
    Handled pymysql.err.InterfaceError:

--- leaderboard.py
+++ leaderboard.py
@@ -5,19 +5,28 @@ import pandas as pd
 st.write("### ⭐ STOCK SAMPEDE! ⭐")
 st.write("Leaderboard below!")
 def show_entries(entries):
-    df = pd.DataFrame(entries, columns=["player_name", "datetime", "PnL"])
-    styled_df = df.style.background_gradient(
-    cmap="RdYlGn", subset=["PnL"])
-    st.dataframe(styled_df)
+    try:
+        df = pd.DataFrame(entries, columns=["player_name", "datetime", "PnL"])
+        styled_df = df.style.background_gradient(
+        cmap="RdYlGn", subset=["PnL"])
+        st.dataframe(styled_df)
+    except pymysql.err.InterfaceError:
+        st.error("Database temporarily unavailable. Please try again.")
 
 st.write("NOTE: Shows only the latest 1000 entries")
 
-st.write("DB Host:", st.secrets["mysql"]["host"])
+#st.write("DB Host:", st.secrets["mysql"]["host"])
 
 if(st.checkbox(label ="### See 100 ticks leaderboard 🏆")): 
-    entries = mysql_custom_functions.show_leaderboard_of_tick_100()
-    show_entries(entries)
+    try:
+        entries = mysql_custom_functions.show_leaderboard_of_tick_100()
+        show_entries(entries)
+    except pymysql.err.InterfaceError:
+        st.error("Database temporarily unavailable. Please try again.")
 
 if(st.checkbox(label ="### See 300 ticks leaderboard 🏆")):
-    entries = mysql_custom_functions.show_leaderboard_of_tick_300()
-    show_entries(entries)
+    try:
+        entries = mysql_custom_functions.show_leaderboard_of_tick_300()
+        show_entries(entries)
+    except pymysql.err.InterfaceError:
+        st.error("Database temporarily unavailable. Please try again.")
