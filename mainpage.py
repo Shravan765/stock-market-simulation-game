8b00e16d6ca029727caed703467f27dca5af6e26
Shravan765 <142580349+Shravan765@users.noreply.github.com>
2025-07-06 12:15:57 +0530

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import mysql_custom_functions
 
-mysql_custom_functions.create_tables()
 game = st.Page(page="stock_market_sim.py", title="Start Stock Sampede??", icon="📊")
 leaderboard = st.Page(page="leaderboard.py", title="Leaderboard", icon="🏆")
 
